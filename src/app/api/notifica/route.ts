70a82cc9319cc33a3ff10ac8dfd193144355e070
StarUpm <mcanini100@gmail.com>
2026-06-27 12:51:59 +0200

    Update route.ts

--- src/app/api/notifica/route.ts
+++ src/app/api/notifica/route.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string
 )
 
 export async function POST(request: NextRequest) {
